6b0f4e9ccb35bbf0ceec0dfb33a7ba92a1e90890
Serge Rabeson <srg.rabeson@gmail.com>
2022-11-13 12:18:18 +0300

    Create a route to get all available object

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,13 +1,14 @@
 import express from "express";
+
+import Obj from "../database/models/Obj";
+
 const router = express.Router();
 
 /**
  * Get available objects
  */
-router.get("/", (req, res) => {
-  return res.json({
-    message: "hello, world",
-  });
+router.get("/", async (req, res) => {
+  return res.json(await Obj.find({}));
 });
 
 export default router;
