cd73c4b3171f82a40d37eae66b358f6a455dee61
Serge Rabeson <srg.rabeson@gmail.com>
2022-11-14 00:01:34 +0300

    Install and configure cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,6 +7,7 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import { expressjwt } from "express-jwt";
+import cors from "cors";
 
 import indexRouter from "./routes/index";
 import authRouter from "./routes/auth";
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 // Setup routers
 app.use("/", indexRouter);
