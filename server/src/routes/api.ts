e98e2dad4b8bd08e4cd313f51538567b419c2947
Serge Rabeson <srg.rabeson@gmail.com>
2022-11-13 13:39:21 +0300

    Create route and controller for editing an existing object

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import getObjectsController from "../controllers/api/getObjectsController";
 import createObjectController from "../controllers/api/createObjectController";
+import editObjectController from "../controllers/api/editObjectController";
 
 const router = express.Router();
 
@@ -14,5 +15,10 @@ router.get("/", getObjectsController);
  * Create a new object
  */
 router.post("/create", createObjectController);
+
+/**
+ * Edit an existing object
+ */
+router.post("/edit", editObjectController);
 
 export default router;
