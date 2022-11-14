8e213abe9d9125017186011cb73c62cf70fa7385
Serge Rabeson <srg.rabeson@gmail.com>
2022-11-15 00:40:50 +0300

    List all available objects from server

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,3 +1,58 @@
+<script setup lang="ts">
+// Ensure user is logged in
+if (!localStorage.getItem("access_token")) {
+  navigateTo("/login");
+}
+
+const config = useRuntimeConfig();
+
+// Get all available objects
+const { data: objects } = await useFetch<
+  {
+    _id: string;
+    name: string;
+    description: string;
+  }[]
+>(`/api`, {
+  method: "GET",
+  headers: {
+    authorization: `Bearer ${localStorage.getItem("access_token")}`,
+  },
+  baseURL: config.public.apiBase,
+});
+</script>
+
 <template>
-  <div>Home Page</div>
+  <!-- Navbar -->
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <NuxtLink to="/">
+        <button type="button" class="btn btn-dark">
+          <font-awesome-icon icon="fa-solid fa-house" />
+        </button>
+      </NuxtLink>
+
+      <NuxtLink class="navbar-brand" to="/">Objects</NuxtLink>
+
+      <button type="button" class="btn btn-dark">
+        <font-awesome-icon icon="fa-solid fa-right-from-bracket" />
+      </button>
+    </div>
+  </nav>
+
+  <!-- Object list -->
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 py-3">
+        <div class="d-grid gap-3">
+          <div v-for="object in objects" :key="object._id" class="card">
+            <h5 class="card-header">{{ object.name }}</h5>
+            <div class="card-body">
+              <p class="card-text">{{ object.description }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
