ead4bfa4b4361200a32fe5dd5eba3b38c2ce0cb3
Serge Rabeson <srg.rabeson@gmail.com>
2022-11-15 00:50:39 +0300

    Add a link to the create page from the index page

---
+++ client/pages/create.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Add object</div>
+</template>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -26,10 +26,8 @@ const { data: objects } = await useFetch<
   <!-- Navbar -->
   <nav class="navbar navbar-dark bg-dark">
     <div class="container-fluid">
-      <NuxtLink to="/">
-        <button type="button" class="btn btn-dark">
-          <font-awesome-icon icon="fa-solid fa-house" />
-        </button>
+      <NuxtLink to="/" class="btn btn-dark">
+        <font-awesome-icon icon="fa-solid fa-house" />
       </NuxtLink>
 
       <NuxtLink class="navbar-brand" to="/">Objects</NuxtLink>
@@ -45,6 +43,11 @@ const { data: objects } = await useFetch<
     <div class="row">
       <div class="col-12 py-3">
         <div class="d-grid gap-3">
+          <NuxtLink to="/create" class="btn btn-primary">
+            <font-awesome-icon icon="fa-solid fa-plus" />
+            <span class="ms-3">Add Object</span>
+          </NuxtLink>
+
           <div v-for="object in objects" :key="object._id" class="card">
             <h5 class="card-header">{{ object.name }}</h5>
             <div class="card-body">
