e8760a978e24941932431ea304e8332ec79a469a
Serge Rabeson <srg.rabeson@gmail.com>
2022-11-15 00:58:34 +0300

    Create a layout

---
+++ client/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- Navbar -->
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <NuxtLink to="/" class="btn btn-dark">
+        <font-awesome-icon icon="fa-solid fa-house" />
+      </NuxtLink>
+
+      <NuxtLink class="navbar-brand" to="/">Objects</NuxtLink>
+
+      <button type="button" class="btn btn-dark">
+        <font-awesome-icon icon="fa-solid fa-right-from-bracket" />
+      </button>
+    </div>
+  </nav>
+
+  <!-- Variable content -->
+  <slot />
+</template>
